9c429f6757a7615a6c3c9e1c06e402a852aeb32b
Jon Smart <jsmartware@gmail.com>
2020-08-16 14:40:57 -0700

    Changed ItemDetailPage to add Picker

--- XFApp/Views/ItemDetailPage.xaml.cs
+++ XFApp/Views/ItemDetailPage.xaml.cs
@@ -1,15 +1,20 @@
-﻿using System.ComponentModel;
-using Xamarin.Forms;
+﻿using Xamarin.Forms;
 using XFApp.ViewModels;
 
 namespace XFApp.Views
 {
     public partial class ItemDetailPage : ContentPage
     {
+        private ItemDetailViewModel viewModel;
         public ItemDetailPage()
         {
             InitializeComponent();
             BindingContext = new ItemDetailViewModel();
+        }
+        public ItemDetailPage(ItemDetailViewModel viewModel)
+        {
+            InitializeComponent();
+            BindingContext = this.viewModel = viewModel;
         }
     }
 }
