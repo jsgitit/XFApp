c937c6821a8b2fb06372a13a8b40f9044c55f4e0
Jon Smart <jsmartware@gmail.com>
2020-08-16 15:08:48 -0700

    Make ItemDetailPage the main page

--- XFApp/App.xaml.cs
+++ XFApp/App.xaml.cs
@@ -14,7 +14,8 @@ namespace XFApp
             InitializeComponent();
 
             DependencyService.Register<MockDataStore>();
-            MainPage = new AppShell();
+            // MainPage = new AppShell();
+            MainPage = new ItemDetailPage();
         }
 
         protected override void OnStart()
